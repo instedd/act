5ab97928619b64b1ed03c876d24e49c9aa15051a
Matias Garcia Isaia <mgarcia@manas.com.ar>
2014-10-22 15:31:06 -0300

    UserController creates users
    See #1

--- src/main/groovy/org/instedd/act/controllers/UserController.groovy
+++ src/main/groovy/org/instedd/act/controllers/UserController.groovy
@@ -1,9 +1,17 @@
 package org.instedd.act.controllers
 
+import org.instedd.act.models.User
+
 class UserController {
     
+    def users = []
+    
     def index() {
-        return []
+        return users
+    }
+    
+    def create(params) {
+        users.add(new User(params.organization, params.location))
     }
 
 }
--- src/test/groovy/org/instedd/act/controllers/UserControllerTest.groovy
+++ src/test/groovy/org/instedd/act/controllers/UserControllerTest.groovy
@@ -3,12 +3,24 @@ package org.instedd.act.controllers
 import spock.lang.*
 
 class UserControllerTest extends Specification{
+    
+    def controller = new UserController()
+    
     def "controller indexes no users"() {
-        setup:
-        def controller = new UserController()
         when:
         def users = controller.index()
         then:
         users.isEmpty()
+    }
+    
+    def "controller indexes users"() {
+        setup:
+        controller.create([organization: 'msf-spain', location: 'Sierra Leone/Northern Province/Kambia District/Kambia'])
+        when:
+        def users = controller.index()
+        then:
+        users.size() == 1
+        users[0].organization == 'msf-spain'
+        users[0].location == ['Sierra Leone', 'Northern Province', 'Kambia District', 'Kambia']
     }
 }
