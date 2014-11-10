e998b7e60422adaed2ae5d8477e0399415c97c05
Matias Garcia Isaia <mgarcia@manas.com.ar>
2014-11-10 18:49:48 -0300

    Sync news from ACT server to client
    See #12

--- client/src/main/groovy/org/instedd/act/sync/RsyncSynchronizer.groovy
+++ client/src/main/groovy/org/instedd/act/sync/RsyncSynchronizer.groovy
@@ -15,15 +15,24 @@ class RsyncSynchronizer implements DocumentSynchronizer {
     
 	Logger logger = LoggerFactory.getLogger(RsyncSynchronizer.class)
 	
-	String baseCommand = "rsync -avz --remove-source-files"
-    String sourceDir
-    String targetDir
-    String sourceHost
-    String targetHost
+	String baseCommand = "rsync -iaz --remove-source-files"
+	
+	String localOutboxDir
+	String remoteOutboxDir
+	String outboxHost
+	
+	String localInboxDir
+	String remoteInboxDir
+	String inboxHost
 
-	def sourceRoute() { route(sourceHost, sourceDir) }
-	def targetRoute() { route(targetHost, targetDir) }
-	def commandLine() { "${baseCommand} ${sourceRoute()} ${targetRoute()}" }
+	def localOutboxRoute() { route("", localOutboxDir) }
+	def remoteOutboxRoute() { route(outboxHost, remoteOutboxDir) }
+	
+	def localInboxRoute() { route("", localInboxDir) }
+	def remoteInboxRoute() { route(inboxHost, remoteInboxDir) }
+	
+	def uploadCommandLine() { "${baseCommand} ${localOutboxRoute()} ${remoteOutboxRoute()}" }
+	def downloadCommandLine() { "${baseCommand} ${localInboxRoute()} ${remoteInboxRoute()}" }
 	
 	RsyncSynchronizer() {
 		checkRsyncAvailable()
@@ -37,21 +46,32 @@ class RsyncSynchronizer implements DocumentSynchronizer {
 			dataStore.saveDeviceIdentifier(UUID.randomUUID().toString());
 		}
 		
-		sourceDir  = settings.get("sync.sourceDir")
-        new File(sourceDir).mkdirs()
-		sourceHost = settings.get("sync.sourceHost")
-		targetDir  = "${settings.get("sync.targetDir")}/${dataStore.deviceIdentifier}"
-		targetHost = settings.get("sync.targetHost")
+		localOutboxDir = settings.get("sync.outbox.localDir")
+		new File(localOutboxDir).mkdirs()
+		remoteOutboxDir = "${settings.get("sync.outbox.remoteDir")}/${dataStore.deviceIdentifier}"
+		outboxHost = settings.get("sync.outbox.remoteHost")
 		
-		logger.info("Will sync files in ${sourceRoute()} to ${targetRoute()}")
+		logger.info("Will sync files from ${localOutboxRoute()} to ${remoteOutboxRoute()}")
+		
+		localInboxDir = settings.get("sync.inbox.localDir")
+		new File(localInboxDir).mkdirs()
+		remoteInboxDir = "${settings.get("sync.inbox.remoteDir")}/${dataStore.deviceIdentifier}"
+		inboxHost = settings.get("sync.inbox.remoteHost")
+		
+		logger.info("Will sync files from ${remoteInboxRoute()} to ${localInboxRoute()}")
 	}
 		
 	@Override
 	public synchronized void syncDocuments() {
+		this.uploadDocuments()
+		this.downloadDocuments()
+	}
+	
+	public synchronized void uploadDocuments() {
         def stdoutBuffer = new StringBuffer()
         def stderrBuffer = new StringBuffer()
         
-        def command = this.commandLine()
+        def command = this.uploadCommandLine()
         logger.debug("Running rsync: {}", command) 
         
         Process process = command.execute()
@@ -67,12 +87,45 @@ class RsyncSynchronizer implements DocumentSynchronizer {
 			logger.warn("Error output for rsync was:\n{}", stderr)
 		}
 	}
+	
+	public synchronized void downloadDocuments() {
+		def stdoutBuffer = new StringBuffer()
+		def stderrBuffer = new StringBuffer()
+		
+		def command = this.downloadCommandLine()
+		logger.debug("Running rsync: {}", command)
+		
+		Process process = command.execute()
+		process.consumeProcessOutput(stdoutBuffer, stderrBuffer)
+		process.waitFor()
+
+		String stdout = stdoutBuffer.toString()
+		String stderr = stderrBuffer.toString()
+		if (StringUtils.isEmpty(stderr)) {
+			logger.trace("Standard output for rsync was:\n{}", stdout)
+		} else {
+			logger.warn("Standard output for rsync was:\n{}", stdout)
+			logger.warn("Error output for rsync was:\n{}", stderr)
+		}
+		
+		def inlines = stdout.readLines().findAll({ line ->
+			line.startsWith(">")
+		})
+		def receivedFilenames = inlines.collect({ line -> line.split(" ", 2)[1]})
+		if(!receivedFilenames.empty) {
+			this.onFilesReceived(receivedFilenames)
+		}
+	}
 
 	@Override
 	public void queueForSync(String documentName, String content) {
-		new File(sourceDir, documentName).withWriter('UTF-8') { out ->
+		new File(localOutboxDir, documentName).withWriter('UTF-8') { out ->
 			out.writeLine(content)
 		}
+	}
+	
+	def onFilesReceived(filenames) {
+		filenames.each { filename -> println "Received ${filename}" }
 	}
 	
 	void checkRsyncAvailable() {
--- client/src/test/groovy/org/instedd/act/sync/RsyncSynchronizerTest.groovy
+++ client/src/test/groovy/org/instedd/act/sync/RsyncSynchronizerTest.groovy
@@ -9,32 +9,32 @@ import spock.lang.*
 class RsyncSynchronizerTest extends GroovyTestCase {
 
     void "ignore: run synchronizer"() {
-        new RsyncSynchronizer([sourceDir: '/tmp/dir1/', targetDir: '/tmp/dir2']).syncDocuments()
+        new RsyncSynchronizer([localOutboxDir: '/tmp/dir1/', remoteOutboxDir: '/tmp/dir2']).syncDocuments()
     }
 
     void "test supports only dirs"() {
-        RsyncSynchronizer rsync = new RsyncSynchronizer([sourceDir: 'src/', targetDir: 'tmp/'])
-        assert rsync.commandLine() == "rsync -avz --remove-source-files src/ tmp/"
+        RsyncSynchronizer rsync = new RsyncSynchronizer([localOutboxDir: 'src/', remoteOutboxDir: 'tmp/'])
+        assert rsync.uploadCommandLine() == "rsync -iaz --remove-source-files src/ tmp/"
     }
 	
 	void "test adds trailing slash to sourceDir if not present to avoid recreating root directory on sync"() {
-		RsyncSynchronizer rsync = new RsyncSynchronizer([sourceDir: 'src', targetDir: 'tmp/'])
-		assert rsync.commandLine() == "rsync -avz --remove-source-files src/ tmp/"
+		RsyncSynchronizer rsync = new RsyncSynchronizer([localOutboxDir: 'src', remoteOutboxDir: 'tmp/'])
+		assert rsync.uploadCommandLine() == "rsync -iaz --remove-source-files src/ tmp/"
 	}
 
     void "test with remote host"() {
-        RsyncSynchronizer rsync = new RsyncSynchronizer([sourceDir: 'src/', targetDir: 'tmp/', targetHost: 'someone@example.com'])
-        assert rsync.commandLine() == "rsync -avz --remove-source-files src/ someone@example.com:tmp/"
+        RsyncSynchronizer rsync = new RsyncSynchronizer([localOutboxDir: 'src/', remoteOutboxDir: 'tmp/', outboxHost: 'someone@example.com'])
+        assert rsync.uploadCommandLine() == "rsync -iaz --remove-source-files src/ someone@example.com:tmp/"
     }
 
     void "test with local and remote host"() {
-        RsyncSynchronizer rsync = new RsyncSynchronizer([sourceDir: 'src/', sourceHost: 'myself@192.168.1.4', targetDir: 'tmp/', targetHost: 'someone@example.com'])
-        assert rsync.commandLine() == "rsync -avz --remove-source-files myself@192.168.1.4:src/ someone@example.com:tmp/"
+        RsyncSynchronizer rsync = new RsyncSynchronizer([localOutboxDir: 'src/', remoteOutboxDir: 'tmp/', outboxHost: 'someone@example.com'])
+        assert rsync.uploadCommandLine() == "rsync -iaz --remove-source-files src/ someone@example.com:tmp/"
     }
 
     void "test custom command"() {
-        RsyncSynchronizer rsync = new RsyncSynchronizer([baseCommand: 'rsync', sourceDir: 'src/', sourceHost: 'myself@192.168.1.4', targetDir: 'tmp/', targetHost: 'someone@example.com'])
-        assert rsync.commandLine() == "rsync myself@192.168.1.4:src/ someone@example.com:tmp/"
+        RsyncSynchronizer rsync = new RsyncSynchronizer([baseCommand: 'rsync', localOutboxDir: 'src/', remoteOutboxDir: 'tmp/', outboxHost: 'someone@example.com'])
+        assert rsync.uploadCommandLine() == "rsync src/ someone@example.com:tmp/"
     }
 
 }
