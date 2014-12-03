cd0b06e11bb3d109f94dbcf60af48e1fce25cc43
Juan Edi <jedi@manas.com.ar>
2014-12-03 18:33:00 -0300

    Changed new case form layout so that input fields appear aligned

--- client/src/main/groovy/org/instedd/act/ui/NewCaseForm.groovy
+++ client/src/main/groovy/org/instedd/act/ui/NewCaseForm.groovy
@@ -2,17 +2,17 @@ package org.instedd.act.ui
 
 import java.awt.BorderLayout
 import java.awt.Button
-import java.awt.Color
 import java.awt.Component
 import java.awt.Dialog
 import java.awt.Dimension
-import java.awt.FlowLayout
-import java.awt.Label
+import java.awt.GridBagConstraints as GBC
+import java.awt.GridBagLayout
 import java.awt.TextField
 
 import javax.swing.BorderFactory
 import javax.swing.BoxLayout
 import javax.swing.ButtonGroup
+import javax.swing.JButton
 import javax.swing.JCheckBox
 import javax.swing.JComboBox
 import javax.swing.JDialog
@@ -92,109 +92,125 @@ class NewCaseForm extends JDialog {
         controller.newCaseSubmitted()
     }
     
-    def build() {
-        JPanel panel = new JPanel()
-        panel.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS))
-        
-        def subpanel = new JPanel()
-        subpanel.setLayout(new BoxLayout(subpanel, BoxLayout.X_AXIS))
-        subpanel.add(new Label('Name'))
-        nameField = new TextField()
-        subpanel.add(nameField)
-        panel.add(subpanel)
-        
-        subpanel = new JPanel()
-        subpanel.setLayout(new BoxLayout(subpanel, BoxLayout.X_AXIS))
-        subpanel.add(new Label('Phone'))
-        phoneField = new TextField()
-        subpanel.add(phoneField)
-        panel.add(subpanel)
-        
-        subpanel = new JPanel()
-        subpanel.setLayout(new BoxLayout(subpanel, BoxLayout.X_AXIS))
-        subpanel.add(new Label('Age'))
-        ageField = new TextField()
-        subpanel.add(ageField)
-        panel.add(subpanel)
-        
-        subpanel = new JPanel()
-        subpanel.setLayout(new BoxLayout(subpanel, BoxLayout.X_AXIS))
-        subpanel.add(new Label('Gender'))
-        
-        genderButtons = new ButtonGroup()
-        ["Male", "Female"].each { gender ->
-            def button = new JRadioButton(gender)
-            genderButtons.add(button)
-            subpanel.add(button)
-        }
-        panel.add(subpanel)
-        
-        subpanel = new JPanel()
-        subpanel.setLayout(new BoxLayout(subpanel, BoxLayout.X_AXIS))
-        subpanel.add(new Label('Dialect preference'))
-        
-        dialectCombo = new JComboBox(controller.availableDialects())
-        dialectCombo.setSelectedIndex(-1)
-        subpanel.add(dialectCombo)
-        panel.add(subpanel)
-        
-        subpanel = new JPanel()
-        subpanel.setLayout(new BoxLayout(subpanel, BoxLayout.X_AXIS))
-        subpanel.add(new Label('Reason'))
-        
-        
-        def reasonsPanel = new JPanel()
-        reasonsPanel.setLayout(new BoxLayout(reasonsPanel, BoxLayout.Y_AXIS))
-        reasonsChecks = []
-        Case.CONTACT_REASONS.each { reason ->
-            JCheckBox reasonCheck = new JCheckBox(reason)
-            reasonsPanel.add(reasonCheck)
-            reasonsChecks.add(reasonCheck)
-        }
-        subpanel.add(reasonsPanel)
-        panel.add(subpanel)
-        
-        subpanel = new JPanel(new FlowLayout(FlowLayout.LEFT))
-        subpanel.add(new JLabel("Notes"))
-        panel.add(subpanel)
-        
-        subpanel = new JPanel()
-        subpanel.setLayout(new BorderLayout(10, 10))
-        
-        notesField = new JTextArea()
-        notesField.setRows(3)
-        notesField.setLineWrap(true)
-        notesField.setWrapStyleWord(true)
-        notesDocument = new DefaultStyledDocument()
-        notesDocument.setDocumentFilter(new DocumentSizeFilter(1000))
-        notesField.setDocument(notesDocument)
-        
-        def scroll = new JScrollPane(notesField)
-        subpanel.add(scroll)
-        panel.add(subpanel)
-        
-        
-        subpanel = new JPanel()
-        addCaseButton = new Button('Add case')
-        addCaseButton.addActionListener({
-            addNewCase()
-        })
-        subpanel.add(addCaseButton)
-        panel.add(subpanel)
-        
-        subpanel = new JPanel()
-        subpanel.setLayout(new BorderLayout(10, 10))
-        messagesField = new JLabel("<html></html>")
-        subpanel.add(messagesField, BorderLayout.CENTER)
-        subpanel.setPreferredSize(new Dimension(80, 50))
-        panel.add(subpanel)
-        
-        add(panel)
-        pack()
-        this.visible = true
-    }
-    
+	def build() {
+		def container = new JPanel()
+		container.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
+		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS))
+		
+		def form = createForm()
+		container.add form
+		
+		def messages = new JPanel()
+		messages.setPreferredSize(new Dimension(80, 50))
+		messages.setLayout(new BorderLayout(10, 10))
+		messagesField = new JLabel("<html></html>")
+		messages.add(messagesField)
+		container.add messages
+		
+		def submitButton = new JButton("Add case")
+		submitButton.alignmentX = Component.CENTER_ALIGNMENT
+		submitButton.addActionListener({ addNewCase() })
+		container.add submitButton
+		
+		add(container)
+		pack()
+		visible = true
+	}
+	
+	def createForm() {
+		def form = new JPanel(new GridBagLayout())
+		
+		addField form, "Name", (nameField = new TextField())
+		addField form, "Phone",(phoneField = new TextField())
+		addField form, "Age",  (ageField = new TextField())
+		addField form, "Gender", createGenderButtons()
+		addField form, "Dialect preference", createDialectCombo()
+		addReasonsField form
+		addNotesField form
+		
+		form
+	}
+	
+	def createDialectCombo() {
+		dialectCombo = new JComboBox(controller.availableDialects())
+		dialectCombo.setSelectedIndex(-1)
+		dialectCombo
+	}
+	
+	def createGenderButtons() {
+		def buttonsContainer = new JPanel()
+		buttonsContainer.setLayout(new BoxLayout(buttonsContainer, BoxLayout.X_AXIS))
+		genderButtons = new ButtonGroup()
+		["Male", "Female"].each { gender ->
+			def button = new JRadioButton(gender)
+			genderButtons.add(button)
+			buttonsContainer.add button
+		}
+		buttonsContainer
+	}
+	
+	def addField(JPanel form, String labelText, Component input) {
+		def c = new GBC()
+		c.gridwidth = GBC.RELATIVE
+		c.anchor = GBC.WEST
+		c.ipady = 10
+		c.ipadx = 5
+		
+		def label = new JLabel(labelText)
+		form.add(label, c)
+
+		c = new GBC()
+		c.weightx = 1
+		c.gridwidth = GBC.REMAINDER
+		c.fill = GBC.HORIZONTAL
+		
+		form.add(input, c)
+	}
+	
+	def addReasonsField(JPanel form) {
+		def c = new GBC()
+		c.gridwidth = GBC.REMAINDER
+		c.anchor = GBC.WEST
+		c.ipady = 10
+		c.ipadx = 40
+		
+		def label = new JLabel("Reasons")
+		form.add(label, c)
+		
+		def reasonsPanel = new JPanel()
+		reasonsPanel.setLayout(new BoxLayout(reasonsPanel, BoxLayout.Y_AXIS))
+		reasonsChecks = []
+		Case.CONTACT_REASONS.each { reason ->
+			JCheckBox reasonCheck = new JCheckBox(reason)
+			reasonsPanel.add(reasonCheck)
+			reasonsChecks.add(reasonCheck)
+		}
+		form.add reasonsPanel, c
+	}
+	
+	def addNotesField(JPanel form) {
+		def c = new GBC()
+		c.gridwidth = GBC.REMAINDER
+		c.anchor = GBC.WEST
+		c.ipady = 10
+		c.ipadx = 40
+		
+		def label = new JLabel("Notes")
+		form.add label, c
+		
+		c.fill = GBC.BOTH
+		notesField = new JTextArea()
+		notesField.setRows(3)
+		notesField.setLineWrap(true)
+		notesField.setWrapStyleWord(true)
+		notesDocument = new DefaultStyledDocument()
+		notesDocument.setDocumentFilter(new DocumentSizeFilter(1000))
+		notesField.setDocument(notesDocument)
+		def scroll = new JScrollPane(notesField)
+		
+		form.add scroll, c
+	}
+	
     def clearMessage() {
         messagesField.text = ''
     }
