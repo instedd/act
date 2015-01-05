70ac02d81cf99d36b12f92abed4a382a9329ecbc
Matias Garcia Isaia <mgarcia@manas.com.ar>
2015-01-05 14:20:45 -0300

    Sync cases files
    Fixes #48

--- client/src/main/groovy/org/instedd/act/sync/DocumentSynchronizer.java
+++ client/src/main/groovy/org/instedd/act/sync/DocumentSynchronizer.java
@@ -1,9 +1,13 @@
 package org.instedd.act.sync;
 
+import java.io.File;
+
 
 public interface DocumentSynchronizer {
 
 	public void syncDocuments();
 	
 	public void queueForSync(String documentName, String content);
+	
+	public void queueForSync(File document);
 }
