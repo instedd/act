317c645f272c4f182a59933b968ebc3223738d1a
Matias Garcia Isaia <mgarcia@manas.com.ar>
2015-02-04 11:55:19 -0300

    Integrate NNDD to docker build
    Fixes #57

--- server/deploy/04_migrate_db.sh
+++ server/deploy/04_migrate_db.sh
@@ -4,5 +4,5 @@ set -e
 
 if (bundle exec rake db:version &> /dev/null);
   then bundle exec rake db:migrate;
-  else bundle exec rake db:setup;
+  else bundle exec rake db:setup act:locations:hierarchy;
 fi;
