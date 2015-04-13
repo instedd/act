7425a53dbb67d2da3690c10427a876342d94166e
Matias Garcia Isaia <mgarcia@manas.com.ar>
2015-04-13 17:59:14 -0300

    Webapp shows application version
    See #87

---
+++ server/config/initializers/version.rb
@@ -0,0 +1,14 @@
+RevisionFilePath = "#{::Rails.root.to_s}/REVISION"
+VersionFilePath = "#{::Rails.root.to_s}/VERSION"
+
+if FileTest.exists?(VersionFilePath)
+  version = IO.read(VersionFilePath)
+elsif FileTest.exists?(RevisionFilePath)
+  version = IO.read(RevisionFilePath)
+else
+  version = "development"
+end
+
+Rails.application.configure do
+  config.act_version = version
+end
